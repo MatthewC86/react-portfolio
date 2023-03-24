0476759fecfdc945655bedc609d0cddd5611bf25
Matthew C <cnymhw86@gmail.com>
2023-03-24 15:28:31 -0400

    first about me commit

---
+++ src/components/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p>
+        My name is Mattew Courtney and have always had a passion for technology.
+        I acquired an associates degree in Systems Developement and Programming
+        in 2013, loving every aspect. I'm currently employed with PNC Bank, as
+        an Online Banking Support specialist and have been with the company for
+        almost 9 years. I love dogs, my family, technology and billiards. I hope
+        to obtain a role where I'm recognized for quality work by my peers.
+      </p>
+    </div>
+  );
+}
+
+export default About;
